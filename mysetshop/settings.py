7189e0f88a57abce226fbdaf5c8604db0d1fdcd6
Vijay Sonawane <vjsonawane8530@gmail.com>
2022-07-20 03:23:41 +0530

    2final commit

--- mysetshop/settings.py
+++ mysetshop/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT	= BASE_DIR / 'media'
 
